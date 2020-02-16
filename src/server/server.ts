c12a71d218d1a7e5454ccfb25cc69349d3f4e951
Thomas Chang <thomascsd808@gmail.com>
2020-02-16 11:21:55 +0800

    server: user routing-controller and typedi

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,48 +1,45 @@
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import * as methodOverride from 'method-override';
-import * as logger from 'morgan';
-import * as path from 'path';
-import ApiRouter from './routes/api-router';
+import * as express from "express";
+import { useExpressServer, useContainer } from "routing-controllers";
+import { Container } from "typedi";
+import * as logger from "morgan";
+import * as path from "path";
+import ApiRouter from "./routes/api-router";
 
 export default class Server {
   public app: express.Application;
-  private distFolder = path.join(__dirname, '..', 'client');
+  private distFolder = path.join(__dirname, "..", "client");
 
   constructor() {
     this.app = express();
     this.config();
     this.route();
-    this.api();
+    this.setControllers();
   }
 
   public config() {
-    this.app.set('view engine', 'html');
-    this.app.set('views', 'src');
-    this.app.use(logger('dev'));
-    this.app.use(bodyParser.json());
-    this.app.use(methodOverride());
+    this.app.set("view engine", "html");
+    this.app.set("views", "src");
+    this.app.use(logger("dev"));
 
     // Server static files from /dist
-    this.app.get('*.*', express.static(this.distFolder));
+    this.app.get("*.*", express.static(this.distFolder));
   }
 
   public route() {
-    this.app.get('*', (req, res, next) => {
-      if (req.url.indexOf('/api') !== -1) {
+    this.app.get("*", (req, res, next) => {
+      if (req.url.indexOf("/api") !== -1) {
         next();
       } else {
-        res.sendFile(path.join(this.distFolder, 'index.html'));
+        res.sendFile(path.join(this.distFolder, "index.html"));
       }
     });
   }
 
-  public api() {
-    const router: express.Router = express.Router();
-    const apiRouter: ApiRouter = new ApiRouter();
-
-    apiRouter.setRouter(router);
-    this.app.use('/api', router);
+  public setControllers() {
+    useExpressServer(this.app, {
+      routePrefix: "api",
+      controllers: []
+    });
   }
 
   public run(port: number) {
