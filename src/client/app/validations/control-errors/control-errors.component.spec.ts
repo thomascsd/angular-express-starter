bd93f2052dba0b0740e0820adfd2d4e8e27cbf61
thomascsd <thomascsd808@gmail.com>
2020-12-31 15:40:22 +0800

    v11

--- src/client/app/validations/control-errors/control-errors.component.spec.ts
+++ src/client/app/validations/control-errors/control-errors.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ControlErrorsComponent } from './control-errors.component';
 
@@ -6,7 +6,7 @@ describe('ControlErrorsComponent', () => {
   let component: ControlErrorsComponent;
   let fixture: ComponentFixture<ControlErrorsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ControlErrorsComponent ]
     })
